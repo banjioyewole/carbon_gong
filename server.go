d8f8f9938594fb29cb3d9baf1b8bbe45c2d2f949
Banji Oyewole <efarrari@gmail.com>
2019-07-31 21:42:16 -0700

    Uploaded the current server.go and the html file

--- server.go
+++ server.go
@@ -1,53 +1,93 @@
 package main
 import (
-	"net/http"
-	"strings"
-	
-	"fmt"
-	"github.com/stianeikeland/go-rpio"
-	"os"
-	"time"
+    "html/template"
+    "net/http"
+    "fmt"
+    "github.com/stianeikeland/go-rpio"
+    "os"
+    "time"
 )
 
 var (
-	// Use mcu pin 8, corresponds to physical pin 17 on the pi
-	pin = rpio.Pin(17)
+    // Use mcu pin 8, corresponds to physical pin 17 on the pi
+    pin = rpio.Pin(17)
+    gonging = false
+    tpl *template.Template
 )
 
-var gonging = false
-func gong(){
-	if !gonging {
-		gonging = true
-		fmt.Println("gong!")
-		pin.High()
-		time.Sleep(time.Second / 10)
-		pin.Low()
-		time.Sleep(time.Second / 10)
-		gonging = false
-	}
+func init() {
+    tpl = template.Must(template.ParseFiles("gong.gohtml"))
 }
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
-	message = strings.TrimPrefix(message, "/")
-	helloText := "You've reached the GongServer."
-	w.Write([]byte(helloText))
-	if(message == "gong"){
-		go gong()
-	}
-}
 func main() {
-	if err := rpio.Open(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+    if err := rpio.Open(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 
-	defer rpio.Close()
+    defer rpio.Close()
 
-	pin.Output()
-	
-	http.HandleFunc("/", sayHello)
-	if err := http.ListenAndServe(":4664", nil); err != nil {
-		panic(err)
-	}
+    pin.Output()
+    
+    http.HandleFunc("/", SayHello)
+    http.HandleFunc("/gong", HandleGong)
+
+    if err := http.ListenAndServe(":4664", nil); err != nil {
+        panic(err)
+    }
 }
+
+func SayHello(w http.ResponseWriter, r *http.Request) {
+    helloText := "You've reached the GongServer."
+    w.Write([]byte(helloText))
+}
+
+func HandleGong(w http.ResponseWriter, r *http.Request) {
+    timeMessage, isOkToGong := isTimeOk()
+    if r.Method == http.MethodPost {
+        if isOkToGong {
+            go gong()
+        }
+        w.WriteHeader(http.StatusBadRequest) // to throw the bots off
+    } else {
+        responseText := fmt.Sprintf("You've reached the GongServer. %s", timeMessage)
+
+        templateData := struct {
+            Text string
+            ShowGongButton bool
+        } {
+            responseText,
+            isOkToGong,
+        }
+        w.Header().Set("Content-Type", "text/html")
+        tpl.ExecuteTemplate(w, "gong.gohtml", templateData)
+    }
+}
+
+func isTimeOk() (string, bool) {
+    earlyLateBoundary := 4
+    start := 9
+    end := 23
+    currentTime := time.Now()
+
+    if currentTime.Hour() >= end || currentTime.Hour() < earlyLateBoundary {
+        return "Sorry, we don't gong this late.", false
+    } else if currentTime.Hour() < start {
+        return "Sorry, we don't gong this early.", false
+    } else {
+        return "", true
+    }
+}
+
+func gong() {
+    if !gonging {
+        gonging = true
+        fmt.Println("gong!")
+        pin.High()
+        time.Sleep(time.Second / 10)
+        pin.Low()
+        time.Sleep(time.Second / 10)
+        gonging = false
+    }
+}
+
